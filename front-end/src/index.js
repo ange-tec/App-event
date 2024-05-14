9149d3798b1d6a72e1c3f455e3efe7f78a0a1654
root <root@Kabod>
2024-05-14 14:22:07 +0200

    front-end modif-2

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,4 @@
 import Router from './Router';
-import Search from './controllers/Search';
-
 import './index.scss';
 
-const routes = [{
-  url: '/search',
-  controller: Search
-}];
-
-new Router(routes);
+new Router();
